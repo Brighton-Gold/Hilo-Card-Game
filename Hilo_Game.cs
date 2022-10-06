d5983981df66bd4d4c27d787acdbf83b5dc7c4f8
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2022-10-06 15:05:33 -0600

    Update to Add or Subtract

--- Hilo_Game.cs
+++ Hilo_Game.cs
@@ -6,74 +6,41 @@ class hilo
         int Points = 300;
         int FinalPoints = 300;
 
+        AddOrSubbtractPoints AddOrSub = new AddOrSubbtractPoints();
+        
+        Deck deck = new Deck();
+
+
         Console.WriteLine("Hello welcome to Hilo! The game is simple, you start with 300 points ",
         "the system will show you card and you have to guess if the next card is higher or lower.",
         "If you are correct, you win points! Enter H if you think it's higher or L if lower. Get ready to play! ");
 
+        /// filler until we make call class for card. After info has passed to here, display card. 
+        /// Call card deck. Card deck will give a card and value will be added to show it has been drawn.
+
+        /// DisplayCard <- card given
+        
+        deck.DrawCard();
+
         while (FinalPoints != 0)
         {
-            int UpdatedPoints = AddOrSubtractPoints(Points);
+            // Display Card. 
+            // Get New Card. 
+
+            int PreviousCard = DisplayCard;
+
+
+            int UpdatedPoints = AddOrSub.ScoreCard(Points, NewCard, PreviousCard);
             FinalPoints = KeepTheGameLooping(UpdatedPoints);
+
+            DisplayCard = NewCard;
         }
 
     }
 
 
-    public static int AddOrSubtractPoints(int Points)
-    {
 
-        /// filler until we make call class for card. After info has passed to here, display card. 
-        /// Might need another function and simply pass the values to this function. 
-        /// PreviousCard <- DisplayCard
-
-        Console.WriteLine("-");
-        string UserGuess = Console.ReadLine();
-        UserGuess = UserGuess.ToUpper();
-
-        /// Get new card. 
-
-        switch (UserGuess)
-        {
-            case "L":
-                if (NewCard < PreviousCard)
-                {
-                    Points = Points + 100;
-                }
-                else
-                {
-                    Points = Points - 75;
-                }
-
-                break;
-
-
-            case "H":
-
-                if (NewCard > Previous)
-                {
-                    Points = Points + 100;
-                }
-
-                else
-                {
-                    Points = Points - 75;
-                }
-
-                break;
-
-            default:
-
-                Console.WriteLine("Input does not work. Try again.");
-                break;
-        }
-        /// previous card <- new card
-
-        return Points;
-
-    }
-
-
-    public static int KeepTheGameLooping(int Points)
+    public int KeepTheGameLooping(int Points)
     {
         Console.WriteLine($"Currently you have {Points} points.");
         Console.WriteLine("Keep playing? Y/N");
@@ -81,7 +48,8 @@ class hilo
         string ContinueGame = Console.ReadLine();
         ContinueGame = ContinueGame.ToUpper();
 
-        /// If player does not wish to continue the points will equal zero, thus ending the while loop. 
+        // If player does not wish to continue the points will equal zero, thus ending the while loop. 
+        // If none of the requirements are met, points should be returned as normal. 
 
         if (ContinueGame == "N")
         {
--- Points.cs
+++ Points.cs
@@ -1,5 +1,56 @@
 
-class AddOrSubbtractPoints{
+class AddOrSubbtractPoints
+{
+    public int ScoreCard(int Points, int NewCard, int PreviousCard)
+    {
+
+        Console.WriteLine("-");
+        string UserGuess = Console.ReadLine();
+        UserGuess = UserGuess.ToUpper();
+
+        /// Get new card. 
+
+        switch (UserGuess)
+        {
+            case "L":
+                if (NewCard < PreviousCard)
+                {
+                    Points = Points + 100;
+                }
+                else
+                {
+                    Points = Points - 75;
+                }
+
+                break;
+
+
+            case "H":
+
+                if (NewCard > PreviousCard)
+                {
+                    Points = Points + 100;
+                }
+
+                else
+                {
+                    Points = Points - 75;
+                }
+
+                break;
+
+            default:
+
+                Console.WriteLine("Input does not work. Try again.");
+                break;
+        }
+        /// previous card <- new card
+
+        return Points;
+
+    }
+
+
 
 
 
